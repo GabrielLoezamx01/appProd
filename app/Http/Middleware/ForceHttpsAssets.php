4d15f9074a30e44c0eed4b8dff566de0c31f2534
Gilberto Us <gilberto@softrek.mx>
2024-02-15 21:38:16 -0600

    class ForceHttpsAssets

--- app/Http/Middleware/ForceHttpsAssets.php
+++ app/Http/Middleware/ForceHttpsAssets.php
@@ -3,24 +3,18 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\URL;
-use Symfony\Component\HttpFoundation\Response;
 
 class ForceHttpsAssets
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
-         // Verifica si la solicitud está en HTTPS
-         if ($request->secure()) {
+        // Verifica si la solicitud está en HTTPS
+        if ($request->secure()) {
             // Modifica la URL base para los activos compilados
             URL::forceScheme('https');
         }
+
         return $next($request);
     }
 }
