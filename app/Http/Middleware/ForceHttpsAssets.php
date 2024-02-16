af7189be890396825165987ccad46a9f5d1d9863
Gilberto Us <gilberto@softrek.mx>
2024-02-15 21:27:41 -0600

    foce https en assets

---
+++ app/Http/Middleware/ForceHttpsAssets.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceHttpsAssets
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+         // Verifica si la solicitud está en HTTPS
+         if ($request->secure()) {
+            // Modifica la URL base para los activos compilados
+            URL::forceScheme('https');
+        }
+        return $next($request);
+    }
+}
