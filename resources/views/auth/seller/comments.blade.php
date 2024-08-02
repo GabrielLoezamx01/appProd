ba2f1bce9b9fc3c61053faff3a8b2a03159da0a7
GabrielLoezamx01 <gabriel26loeza04@gmail.com>
2024-08-02 00:47:00 -0600

    test

--- resources/views/auth/seller/comments.blade.php
+++ resources/views/auth/seller/comments.blade.php
@@ -75,6 +75,9 @@
     </style>
 @endpush
 @section('content')
+
+
+{{-- Hola Mundo --}}
     <div class="row d-flex justify-content-center align-items-center" style="min-height: 100vh; background-color: #D6DFE4;">
         <section class="row justify-content-center col-md-6" style="background-color: #D6DFE4;">
             <div class="p-5 mt-5"></div>
